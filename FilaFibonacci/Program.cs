fbcfdb08d638b24a38960a776a1034ae2b31e2a4
AnnyaB <anabordin2008@gmail.com>
2024-05-03 22:19:57 -0300

    Adicionar arquivos de projeto.

---
+++ FilaFibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FilaFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, n1 = -1, n2 = 1, aux = 0;
+
+            do
+            {
+                Console.WriteLine("\"Digite um número positivo e valido de elementos");
+                numero = int.Parse(Console.ReadLine());
+            } while (numero < 0);
+
+            int[] fibonacci = new int[numero];
+
+            Console.WriteLine("Sequencia Fibonacci:");
+            for (int i = 0; i < numero; i++)
+            {
+                aux = n1 + n2;
+                n1 = n2;
+                n2 = aux;
+                fibonacci[i] = aux;
+            }
+
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write($"{fibonacci[i]}, ");
+            }
+        }
+    }
+}
